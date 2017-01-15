39c0012f62fad59c0a45ee89db3deaf17790c360
Rashed Abdel-Tawab <rashed@linux.com>
2017-01-14 21:00:50 -0500

    h830: Update fingerprint from N
    Change-Id: Icac809ad57508a295fc83e8b3e03795fa049f2b9

--- lineage.mk
+++ lineage.mk
@@ -11,5 +11,5 @@ PRODUCT_NAME := lineage_h830
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="g5" \
     PRODUCT_NAME="h1_tmo_us" \
-    BUILD_FINGERPRINT="lge/h1_tmo_us/h1:6.0.1/MMB29M/1614410155f20:user/release-keys" \
-    PRIVATE_BUILD_DESC="h1_tmo_us-user 6.0.1 MMB29M 1614410155f20 release-keys"
+    BUILD_FINGERPRINT="lge/h1_tmo_us/h1:7.0/NRD90U/1631213003394:user/release-keys" \
+    PRIVATE_BUILD_DESC="h1_tmo_us-user 7.0 NRD90U 1631213003394 release-keys"
