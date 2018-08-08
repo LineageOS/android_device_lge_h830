63a78987b687bf56d71415f68d255fd64266789a
Eric Meddaugh <eric@meddaughs.com>
2018-08-08 07:25:36 -0400

    h830: Add TARGET_SYSTEM_PARTITION for system-as-root layout
    Change-Id: I364e356ea90634b944990b3ed793e07af04f2340

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,6 +21,7 @@ DEVICE_PATH := device/lge/h830
 -include device/lge/g5-common/BoardConfigCommon.mk
 
 TARGET_OTA_ASSERT_DEVICE := g5,h1,h830
+TARGET_SYSTEM_PARTITION := /dev/sda16
 
 # Kernel
 TARGET_KERNEL_CONFIG := lineageos_h830_defconfig
