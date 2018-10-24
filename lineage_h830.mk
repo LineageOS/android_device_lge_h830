83b149e0c54fb2b8be634aa32fd3e92265112aa7
Eric Meddaugh <eric@meddaughs.com>
2018-10-24 11:13:15 -0400

    h830: Consider lineage_h830 product via AOSP way
    Change-Id: I3f92659a58c366bbc529e18eaf1ffcf78b7a6717

--- lineage_h830.mk
+++ lineage_h830.mk
@@ -18,12 +18,23 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from h815 device
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit from h830 device
 $(call inherit-product, device/lge/h830/device.mk)
 
 # Set those variables here to overwrite the inherited values.
 PRODUCT_DEVICE := h830
-PRODUCT_NAME := full_h830
+PRODUCT_NAME := lineage_h830
 PRODUCT_BRAND := lge
 PRODUCT_MODEL := LG-H830
 PRODUCT_MANUFACTURER := LGE
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE="h1" \
+    PRODUCT_DEVICE="h1" \
+    PRODUCT_NAME="h1_tmo_us" \
+    PRIVATE_BUILD_DESC="h1_tmo_us-user 7.0 NRD90U 1719814198c76 release-keys"
+
+BUILD_FINGERPRINT := "lge/h1_tmo_us/h1:7.0/NRD90U/1719814198c76:user/release-keys"
