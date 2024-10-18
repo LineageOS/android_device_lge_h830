e010ef79732a11436d7d743e7a7e10620a217059
LuK1337 <priv.luk@gmail.com>
2024-10-18 11:40:33 +0200

    h830: Adjust override prop key names
    Change-Id: I5a476d8a3ebf0d32cadb4472e0d2dfc028fb37ee

--- lineage_h830.mk
+++ lineage_h830.mk
@@ -32,9 +32,7 @@ PRODUCT_MODEL := LG-H830
 PRODUCT_MANUFACTURER := LGE
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    TARGET_DEVICE="h1" \
-    PRODUCT_DEVICE="h1" \
-    PRODUCT_NAME="h1_tmo_us" \
-    PRIVATE_BUILD_DESC="h1_tmo_us-user 8.0.0 OPR1.170623.032 182251913485f release-keys"
-
-BUILD_FINGERPRINT := "lge/h1_tmo_us/h1:8.0.0/OPR1.170623.032/182251913485f:user/release-keys"
+    BuildDesc="h1_tmo_us-user 8.0.0 OPR1.170623.032 182251913485f release-keys" \
+    BuildFingerprint="lge/h1_tmo_us/h1:8.0.0/OPR1.170623.032/182251913485f:user/release-keys" \
+    DeviceName="h1_tmo_us" \
+    DeviceProduct="h1"
